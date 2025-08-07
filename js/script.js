405b476124acec2e08c3010612312ecfee1b667c
christian-sigel <133788258+christian-sigel@users.noreply.github.com>
2025-08-07 16:57:53 +0200

    sda slides

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@ const slides = document.querySelectorAll('.slide');
 function showNextSlide() {
   const current = slides[currentSlide];
   current.classList.remove('active');
+  current.classList.add('next');
 
   currentSlide = (currentSlide + 1) % slides.length;
   const next = slides[currentSlide];
 
   setTimeout(() => {
     next.classList.add('active');
-  }, 1000);
+    current.classList.remove('next');
+  }, 2000);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   slides[0].classList.add('active');
-  setInterval(showNextSlide, 15000);
+  setInterval(showNextSlide, 10000);
 });
